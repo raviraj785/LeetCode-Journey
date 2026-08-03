60f5ab5d800ae52d1875105fbc3163d009ee7302
Raviraj kumar singh <ravirajkumarsingh40@gmail.com>
2026-08-04 01:06:01 +0530

    Time: 0 ms (100%), Space: 48.4 MB (50.72%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+         int st = 0 ;
+         int end = nums.length -1  ;
+    
+         while(st <= end ){
+
+             int mid = st + (end - st)/2;
+             if (nums[mid] == target ){
+                return mid;
+             }
+             else if (target < nums[mid]){
+                end = mid -1 ;
+             }
+             else {
+                st = mid +1;
+
+             }
+         }
+         return -1;
+    }
+}
