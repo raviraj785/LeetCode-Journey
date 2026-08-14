37922ffa30a38ecac3dae3ebed450c6dbbaedb6d
Raviraj kumar singh <ravirajkumarsingh40@gmail.com>
2026-08-14 19:21:31 +0530

    Time: 0 ms (100%), Space: 47.1 MB (16.23%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -0,0 +1,24 @@
+
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        if(head==null){
+            return false;
+        }
+        if(head.next == null){
+            return false;
+        }
+        while(fast!=null){
+            if(slow == null) return false;
+            slow =  slow.next;
+            if(fast.next == null) return false;
+            fast = fast.next.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+}
