2761359556c69a893a98b2251a0fb8c1dd6271f9
Raviraj kumar singh <ravirajkumarsingh40@gmail.com>
2026-08-22 18:05:15 +0530

    Time: 31 ms (84.12%), Space: 102.5 MB (20.1%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,38 +1,91 @@
-class MinStack {
-    Stack<Integer> st = new Stack<>();
-    Stack<Integer> min = new Stack<>();
+/* Apporach - 01 */
 
+// class MinStack {
+//     Stack<Integer> st = new Stack<>();
+//     Stack<Integer> min = new Stack<>();
+
+//     public MinStack() {
+        
+//     }
+    
+//     public void push(int value) {
+//         if(st.size() == 0){
+//             st.push(value);
+//             min.push(value);
+//         }
+//         else{
+//             st.push(value);
+//             if(min.peek() < value){
+//                 min.push(min.peek());
+//             }
+//             else{
+//                 min.push(value);
+//             }
+//         }
+//     }
+    
+//     public void pop() {
+//         st.pop();
+//         min.pop();
+//     }
+    
+//     public int top() {
+//         return st.peek();
+//     }
+    
+//     public int getMin() {
+//         return min.peek();
+//     }
+// }
+
+/*  Best Apporach  Without extra Stack */
+class MinStack {
+    Stack<Long> st = new Stack<>();
+    long min = -1;
     public MinStack() {
         
     }
     
     public void push(int value) {
+        long  x = (long)value;
         if(st.size() == 0){
-            st.push(value);
-            min.push(value);
+            st.push(x);
+            min=x ;
         }
-        else{
-            st.push(value);
-            if(min.peek() < value){
-                min.push(min.peek());
-            }
-            else{
-                min.push(value);
-            }
-        }
+      else  if(x >= min){
+        st.push(x);
+       }
+       else if (x<min){
+        st.push(2*x-min);
+        min = x;
+       }
+       
     }
     
     public void pop() {
-        st.pop();
-        min.pop();
+       if(st.size() == 0 ) return ;
+      else if(st.peek() >= min) st.pop();
+       else if(st.peek() < min ){
+        // Restore The Old Minimum
+         long old = 2*min - st.peek();
+         min = old;
+         st.pop();
+       }
     }
     
     public int top() {
-        return st.peek();
+       
+       if(st.size() == 0) return -1;
+       long q = st.peek();
+       if(q >= min) return (int)(q);
+       if(q <min) return (int)min;
+      return 0;
     }
     
     public int getMin() {
-        return min.peek();
+        if(st.size() == 0 ) return -1;
+        return (int)min;
     }
 }
+
 
