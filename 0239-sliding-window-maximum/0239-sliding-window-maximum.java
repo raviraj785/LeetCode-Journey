047b813e213d8bea15ea96db4954580efff00916
SK Raviraj <ravirajkumarsingh40@gmail.com>
2026-08-23 23:53:52 +0530

    Time: 516 ms (5.01%), Space: 149.4 MB (39.94%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -13,9 +13,9 @@ class Solution {
             if(st.size() == 0 ) nge[i] = n;
             else nge[i] = st.peek();
             st.push(i);
-        }
+        }int j = 0;
         for(int i = 0 ;i < n -k+1 ;i++){
-            int j = i;
+           if(j >= i+ k) j = i ;
             int max = nums[j];
             while(j < i+k){
                 max = nums[j];
